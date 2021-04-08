9f36dadecfcbbd4b425a654f35addeb15e6ef19c
Alexander <alexander@proximus-apps.com>
2021-04-08 19:11:58 +0200

    added some files for code preview,colors edit

---
+++ code_examples/code.ts
@@ -0,0 +1,13 @@
+enum TEST_ENUM {
+ First,
+ Second
+}
+const TEST_OBJECT = {
+ PROP: 'Value'
+}
+
+const variable = null;
+let bool = true;
+
+console.log(100_000_000);
+TEST_OBJECT.PROP.toLowerCase();
