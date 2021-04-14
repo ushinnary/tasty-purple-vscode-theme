7e2741d161e216d328db7518de5aed933a9f5232
ushinnary <kartushin.alexander97@gmail.com>
2021-04-14 21:24:47 +0200

    took colors for VUE template from github theme

---
+++ code_examples/code.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="code" :title="Obj.prop">
+    {{ Obj.prop }}
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      Obj: { prop: "val" },
+    };
+  },
+  mounted() {
+    const testObj = { prop: "val", val: false };
+    const test = "testing";
+    testObj.prop = "";
+    testObj.val = true;
+    console.log(this.Obj.prop);
+    console.log(testObj.prop);
+    console.log(test);
+  },
+  methods: {
+    test(params) {
+      console.log(params);
+      return true;
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+#id {
+  .class {
+    color: red;
+  }
+}
+</style>
